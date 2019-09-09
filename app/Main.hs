abe280aebc1c7dd7bf61554286fa169021e93ae3
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-09-09 08:37:20 -0400

    switch from postgres --> mysql

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,21 @@ import Application () -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
 import Database.Persist.Sql
-import Database.Persist.Postgresql (withPostgresqlPool)
+import Database.Persist.MySQL (withMySQLPool, defaultConnectInfo, ConnectInfo (..) )
 
 import Control.Monad.Logger
 --import Control.Monad.Trans.Resource (runResourceT)
 
 import Model (migrateAll)
 
-pgConnString :: [Char]
-pgConnString = "host=localhost port=5432 user=postgres dbname=app password=postgres"
+--dbConnString :: ConnectInfo
+--dbConnString = ConnectInfo
+--  { connectHost = "localhost"
+--  , connectPort = 3306
+--  , connectUser = "mysql"
+--  , connectDatabase = "app"
+--  , connectPassword = "mysql"
+--  }
 
 openDbConnectionCount :: Int
 openDbConnectionCount = 10
@@ -20,8 +26,14 @@ openDbConnectionCount = 10
 main :: IO ()
 main =
   runNoLoggingT
-    (withPostgresqlPool
-      "host=localhost port=5432 user=postgres dbname=app password=postgres"
+    (withMySQLPool
+      (defaultConnectInfo
+        { connectHost = "127.0.0.1"
+        , connectPort = 3306
+        , connectUser = "mysql"
+        , connectDatabase = "app"
+        , connectPassword = "mysql"
+        })
       openDbConnectionCount
       (\pool -> (runSqlPool (runMigration migrateAll) pool) >> liftIO (warp 3000 (App pool))))
 
