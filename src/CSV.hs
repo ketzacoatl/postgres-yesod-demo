5a9549c952f80b7f1c1171d82a483dce57b6ed79
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-09-16 23:36:45 -0400

    WIP, processing db entries and JSON there-in to CSV (not yet)

--- src/CSV.hs
+++ src/CSV.hs
@@ -1,12 +1,37 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module CSV where
+
+import           Data.Aeson (eitherDecode, FromJSON, ToJSON)
+import           Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
+import           Data.Csv
+  ( encodeDefaultOrderedByName
+  , DefaultOrdered(headerOrder)
+  , Header
+  , namedRecord
+  , ToNamedRecord(toNamedRecord)
+  , (.=)
+  )
+
+import qualified Data.Csv as CSV
+import qualified Data.Foldable as Foldable
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           GHC.Generics (Generic)
+import           Control.Monad.IO.Class (liftIO)
+
 
 import Data.Aeson (encode)
 import Data.Text (Text)
@@ -15,22 +40,113 @@ import Database.Persist
 import Database.Persist.Sql
 --import Database.Persist.Quasi
 --import Database.Persist.TH
-import Yesod.Persist.Core
+import Yesod.Persist.Core (runDB) --(Entity, runDB, selectList, (!=.))
 
 import Foundation
-import Yesod.Core
+import Yesod.Core (defaultLayout, setTitle, whamlet, Html(..)) --, Handler, Html, whamlet)
 
 import Model
 
 getCSVR :: Handler Html
 getCSVR = do
     persons <- queryPersons
+    writeCsv persons
 
     defaultLayout $ do
-        setTitle "Minimal Multifile"
+        setTitle "select and write to CSV demo"
         [whamlet|
             <p>#{show (persons)}
         |]
   where
     queryPersons :: Handler [Entity Person]
     queryPersons = runDB $ selectList [PersonName !=. ""] []
+
+    writeCsv :: [Entity Person] -> IO ()
+    writeCsv peeps = map processPerson peeps
+
+    processPerson :: (Entity Person) -> [Vector PersonCsv]
+    processPerson p = do
+        let fName = PersonName p
+        let personJsonText = PersonJsonInfo p
+        let personJson = processJsonData (eitherDecode personJsonText :: Either String [PersonJson])
+        let csvRecords = fmap generateCsvPersons fName personJson
+        return csvRecords 
+        
+
+-- #######################
+
+-- read, process, and print out info from the data.json
+--someFunc :: IO ()
+--someFunc = do
+--  f <- BS.readFile "data.json"
+--  print f
+--  let d = processJsonData (eitherDecode f :: Either String [JsonItem])
+--  print d
+--  let csvItems = generateCsvItems d
+--  writeCsvItemsToFile "items.csv" csvItems
+
+--
+
+csvHeader :: CSV.Header
+csvHeader =
+  Vector.fromList
+    [ "Full Name"
+    , "Name"
+    , "Age"
+    ]
+
+data PersonJson = PersonJson
+  { age :: Int
+  , name :: Text
+  } deriving (Show, Generic)
+
+data PersonCsv = PersonCsv
+  { cPersonFullName :: Text
+  , cPersonName :: Text
+  , cPersonAge :: Int
+  } deriving (Show, Generic)
+
+-- use generics
+instance FromJSON PersonJson
+instance ToJSON   PersonJson
+
+instance CSV.DefaultOrdered PersonCsv where
+  headerOrder _ =
+    CSV.header
+      [ "Full Name" -- .= cPersonFullName
+      , "Name" -- .= cPersonName
+      , "Age" -- .= cPersonAge
+      ]
+
+instance CSV.ToNamedRecord PersonCsv where
+  toNamedRecord PersonCsv{..} =
+    CSV.namedRecord
+      [ "Full Name" .= cPersonFullName
+      , "Name" .= cPersonName
+      , "Age" .= cPersonAge
+      ]
+
+-- a little error handling when processing the JSON input data
+processJsonData :: Either a b -> b
+processJsonData (Left _) = error "unable to parse data"
+processJsonData (Right x) = x
+
+generatePersonCsv :: PersonJson -> Text -> PersonCsv
+generatePersonCsv jp fname = PersonCsv
+  { cPersonFullName = fname
+  , cPersonName = name jp
+  , cPersonAge = age jp
+  }
+
+-- IMPLEMENT
+--generateCsvPersons :: [PersonJson] -> Vector PersonCsv
+generateCsvPersons :: [PersonJson] -> Vector PersonCsv
+generateCsvPersons persons = Vector.fromList $ map processPersonToCsvRecords persons
+  where processPersonToCsvRecords p = map generatePersonCsv persons
+
+encodeCsvPersons :: Vector PersonCsv -> ByteString
+encodeCsvPersons = encodeDefaultOrderedByName . Foldable.toList
+
+writeCsvPersonsToFile :: FilePath -> Vector PersonCsv -> IO ()
+writeCsvPersonsToFile filePath =
+  BS.writeFile filePath . encodeCsvPersons
